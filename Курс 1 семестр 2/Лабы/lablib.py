4538b3d89785d8e704e5c85877c6e600ef877ddd
Ivan Shapovalov <intelfx@intelfx.name>
2016-03-03 14:56:06 +0300

    ВУЗ/Курс 1 семестр 2/Лабы/2.4.1: следующая лаба и соответствующие правки lablib.

--- Курс 1 семестр 2/Лабы/lablib.py
+++ Курс 1 семестр 2/Лабы/lablib.py
@@ -59,6 +59,12 @@ def linspace(arg, ticks = 100):
 	min_arg, max_arg = minmax(arg)
 	return np.linspace(min_arg, max_arg + 0.1 * (max_arg - min_arg), ticks)
 
+# computes error of given expression (symbolically) given list of its variables (to consider in the calculation)
+# returns:
+# - error expression
+# - variables representing errors of given variables
+# - list of derivatives of given variables
+# - list of squared error*derivative of given variables
 def sym_error(expr, expr_vars):
 	expr_err_vars = []
 	expr_err_e_d_sq = []
@@ -71,17 +77,29 @@ def sym_error(expr, expr_vars):
 		expr_err_e_d_sq += [(err_deriv*err_var)**2]
 	return smp.sqrt(sum(expr_err_e_d_sq)), expr_err_vars, expr_err_derivs, expr_err_e_d_sq
 
+# computes a substitution dictionary for the .subs() method of the symbolic expression
+# takes:
+# - list of variables to substitute
+# - list of variables representing errors of given variables to substitute
+# - a DataFrame in usual format with variables' data
 def sym_make_subs(expr_vars, expr_err_vars, data):
 	var_pairs = { var: data.Value[var.name]
 	              for var
-	              in expr_vars }
+	              in expr_vars
+	              if var.name in data.Value }
 	err_pairs = { err_var: data.Error[var.name]
 	              for var, err_var
-	              in zip(expr_vars, expr_err_vars) }
+	              in zip(expr_vars, expr_err_vars)
+	              if var.name in data.Error }
 
 	var_pairs.update(err_pairs)
 	return var_pairs
 
+# computes a symbolic expression along with its error from given data
+# takes:
+# - the expression name (for logging)
+# - the expression
+# - a DataFrame in usual format with variables' data
 def sym_compute(name, expr, data):
 	expr_vars = expr.atoms(smp.Symbol)
 	expr_err, expr_err_vars, expr_err_derivs, expr_err_e_d_sq = sym_error(expr, expr_vars)
@@ -94,5 +112,47 @@ def sym_compute(name, expr, data):
 	                  index = ["Error", "Derivative", "(E*D)^2"])
 	disp("Error influence estimations for %s:" % name, bits)
 
-	return var(name, expr.subs(expr_subs), expr_err.subs(expr_subs))
+	return var(name, float(expr.subs(expr_subs)), float(expr_err.subs(expr_subs)))
+# computes a substitution dictionary (template) for the .subs() method of the symbolic expression
+# has column names instead of values
+def sym_make_subs_cols_mapping(expr_vars, expr_err_vars, cols_mapping):
+	var_pairs = { var: cols_mapping[var.name]["Value"]
+	              for var
+	              in expr_vars
+	              if var.name in cols_mapping }
+	err_pairs = { err_var: cols_mapping[var.name]["Error"]
+	              for var, err_var
+	              in zip(expr_vars, expr_err_vars)
+	              if var.name in cols_mapping }
+
+	var_pairs.update(err_pairs)
+	return var_pairs
+
+# data is a DataFrame common constants for all instances of the computation
+# cols_mapping is formatted as input to `var_dict`, except with column names instead of values
+# cols is a DataFrame with columns
+def sym_compute_column(name, expr, data, cols_mapping, cols):
+	expr_vars = expr.atoms(smp.Symbol)
+	expr_err, expr_err_vars, expr_err_derivs, expr_err_e_d_sq = sym_error(expr, expr_vars)
+	expr_subs = sym_make_subs(expr_vars, expr_err_vars, data)
+
+	# pre-substitute constants
+	expr = expr.subs(expr_subs)
+	expr_err = expr_err.subs(expr_subs)
+
+	expr_subs_cols_mapping = sym_make_subs_cols_mapping(expr_vars, expr_err_vars, cols_mapping)
+	expr_subs_column = [ { var: s[col_name]
+	                       for var, col_name
+	                       in expr_subs_cols_mapping.items() }
+	                     for i, s
+	                     in cols.iterrows() ]
+
+	expr_column = [ float(expr.subs(data))
+	                for data
+	                in expr_subs_column ]
+	expr_err_column = [ float(expr_err.subs(data))
+	                    for data
+	                    in expr_subs_column ]
+
+	return expr_column, expr_err_column
 #  vim: set ts=8 sw=8 tw=0 noet ft=python :
